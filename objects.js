5078846c78667b7f315425d22bc434e37584b7c2
selam lemma <oneiconforlife26@gmail.com>
2020-09-27 19:02:48 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,6 @@
 var playlist = {name: "songTitle"}
 
 function updatePlaylist(playlist, artistName, songTitle){
-  playlist.artistName = 'songTitle';
+  playlist[artistName] = 'songTitle';
+  return playlist
 }
